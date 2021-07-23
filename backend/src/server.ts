6f1f97c3311595fd07a4bc18f02c0f19021b7108
yubintw <yubin.cs05g@nctu.edu.tw>
2021-07-23 15:36:46 +0800

    remove exit(0) when fastify fail to start

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,6 @@ const startFastify: (port: number) => FastifyInstance<Server, IncomingMessage, S
     server.listen(port, (err, _) => {
         if (err) {
             console.error(err)
-            process.exit(0)
         }
         establishConnection()
     })
